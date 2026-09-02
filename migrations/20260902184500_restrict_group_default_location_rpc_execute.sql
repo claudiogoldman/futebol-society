5824ae19638923e48886559fc32e389b2c3f29db
claudiogoldman <claudio.goldman@gmail.com>
2026-09-02 15:44:27 -0300

    security: restrict group default location rpc

---
+++ migrations/20260902184500_restrict_group_default_location_rpc_execute.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.set_group_default_location(uuid, uuid) from anon, public;
+grant execute on function public.set_group_default_location(uuid, uuid) to authenticated;
