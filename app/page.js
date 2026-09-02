f31cdb34ed23bf7327f0db4c1bd0ce5a911a7b01
claudiogoldman <claudio.goldman@gmail.com>
2026-09-02 13:43:45 -0300

    refactor: isolate Society page behind entrypoint boundary

--- app/page.js
+++ app/page.js
@@ -1,1 +1,12 @@
-export { default } from './society-page';
+'use client';
+
+import SocietyPage from './society-page';
+import SocietyErrorBoundary from './society-error-boundary';
+
+export default function Page() {
+  return (
+    <SocietyErrorBoundary>
+      <SocietyPage />
+    </SocietyErrorBoundary>
+  );
+}
