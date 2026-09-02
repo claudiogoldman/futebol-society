f4f2a84b6137ede7118dc1d9f61af294c37c1c5c
claudiogoldman <claudio.goldman@gmail.com>
2026-09-01 23:14:52 -0300

    fix: restrict trigger function grants

---
+++ supabase/migrations/20260902021439_restrict_trigger_function_grants.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.apply_game_organizer_defaults() from anon, authenticated;
+revoke execute on function public.ensure_single_default_group_location() from anon, authenticated;
