7069a579541fe824d1f0a178a871dfa8a50a9d32
claudiogoldman <claudio.goldman@gmail.com>
2026-09-12 09:03:01 -0300

    feat(invite): display group rule on invite link

--- app/page.js
+++ app/page.js
@@ -2,10 +2,12 @@
 
 import SocietyPage from './society-page';
 import SocietyErrorBoundary from './society-error-boundary';
+import GroupInviteNotice from './components/group-invite-notice';
 
 export default function Page() {
   return (
     <SocietyErrorBoundary>
+      <GroupInviteNotice />
       <SocietyPage />
     </SocietyErrorBoundary>
   );
